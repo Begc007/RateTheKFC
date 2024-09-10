041f6fbcfc0c01db2379ab35e167e51f3a5e2b21
begc007 <begc007@gmail.com>
2024-09-10 23:09:45 +0500

    Add project files.

---
+++ RateTheKFC/ViewModels/MainWindowVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateTheKFC.ViewModels
+{
+    public class MainWindowVM : INotifyPropertyChanged {
+        private string _searchQuery = string.Empty;
+        public string SearchQuery { 
+            get { return _searchQuery; }
+            set { 
+                _searchQuery = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
